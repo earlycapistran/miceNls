90bd3ddd444c0c6445ea9919c6e42bee41ecc5f2
Michelle <earlycapistran@gmail.com>
2022-05-07 19:58:00 -0500

    Made plotting more efficient

--- plotting_01_cpue_annotated_ggplot.R
+++ plotting_01_cpue_annotated_ggplot.R
@@ -48,36 +48,36 @@ cmydas_na <- cmydas_data %>%
 
 # Experiment: make everything from the single data frame
 gradient_base <- ggplot() +
-  geom_col(cmydas_na %>% 
-             filter(cpue > 0), 
-           mapping = aes(x = year, y= cpue, fill = cpue)) +
+  # Filter positive values to plot CPUE
+  geom_col(
+    cmydas_na %>% 
+      filter(cpue > 0), 
+    mapping = aes(x = year, y= cpue, fill = cpue)
+           ) +
+  # Add scientific colour guide pallette to CPUE values
   scale_fill_scico(palette = 'oslo', begin = 0.2, end = 0.6, direction = -1) +
+  # Plot placeholders for missing data
+  geom_col(
+    cmydas_na %>% 
+      filter(cpue < 0), 
+    mapping = aes(x = year, y= cpue, alpha = "Missing Data")
+    ) +
+  # Alpha scale is a placeholder to generate legend key. Alpha set to 1.
+  scale_alpha_manual(name = NULL, values = c(1)) +
+  # Adjust legend settings
   labs(x = "Year", y = "CPUE (turtles/12 hr)", fill = "CPUE") +
   theme_cmydas() +
   theme(
     legend.position = "right",
     legend.box = "vertical",
-    legend.spacing.y = unit(3, "mm"),
-    axis.title.x=element_blank(), 
-    axis.text.x=element_blank(),
-    axis.ticks.x=element_blank()
+    legend.spacing.y = unit(3, "mm")
   ) +
-  geom_col(cmydas_na %>% 
-             filter(cpue < 0), 
-           mapping = aes(x = year, y= cpue, alpha = "Missing Data")) +
-  scale_alpha_manual(name = NULL, values = c(1)) +
+  # Fix guide order
   guides(
     fill = guide_colourbar(order = 1),
-    alpha = guide_legend(order = 2))
+    alpha = guide_legend(order = 2)
+    )
 gradient_base
-
-gradient_base2 <- gradient_base +
-  geom_col(cmydas_na %>% 
-             filter(cpue < 0), 
-           mapping = aes(x = year, y= cpue, alpha = "Missing Data")) +
-  scale_alpha_manual(name = NULL, values = c(1)) +
-  guides(alpha = guide_legend(reverse=TRUE))
-gradient_base2
 
 # Annotated plot for CPUE -----------------------------------------------------
 
@@ -98,6 +98,41 @@ gradient_base <- ggplot() +
     axis.ticks.x=element_blank()
     )
 gradient_base
+
+# Generate a shaded area to distinguish LEK-derived values 
+gradient_shade <- gradient_base +
+  annotate(
+    "rect", 
+    xmin = 1950, xmax = 1983, ymin = 0, ymax = 20,
+    alpha = 0.75, color="lightgrey", fill="lightgrey"
+  ) 
+gradient_shade
+
+# Move shading layer to background
+gradient_shade <- move_layers(gradient_shade, "GeomRect", position = "bottom")
+gradient_shade
+
+# Annotate dates for key events -----------------------------------------------
+# Label 1: Beginning of permanent sea turtle conservation efforts in BLA
+# Label 2: Nesting beach protection
+# Label 3: Permit closure for C. mydas
+# Label 4: Permanent ban on sea turtle captures in Mexico
+# Label 5: Start of in-water monitoring at BLA
+cpue_plot_annotated <- gradient_shade + 
+  annotate("label", 
+           x = c(1979, 1979, 1983, 1990, 1995), 
+           y = c(8.5, 7, 7, 7, 7), 
+           label = c("1", "2", "3", "4", "5") , 
+           colour = "#363636", fill = "white", 
+           family = "lato", fontface = "bold") +
+  annotate("segment",
+           x = c(1979, 1983, 1990, 1995),
+           xend = c(1979, 1983, 1990, 1995),
+           y = c(6, 6, 6, 6),
+           yend = c(2, 0.5, 0.5,0.65),
+           color = "#363636",
+           arrow = arrow(length = unit(3, "mm"))) 
+cpue_plot_annotated
 
 # Test with scientific colour maps ---
 # View palettes:
